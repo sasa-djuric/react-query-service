f053fd3625d3faeb13a676999acb6a43e1208789
Saša Đurić <djsasa.mail@gmail.com>
2021-03-14 17:25:53 +0100

    Adjusted exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-export {
-	useInfiniteQuery,
-	useIsFetching,
-	useQueries,
-	useQueryClient,
-	useQueryErrorResetBoundary,
-	Query,
-	QueryClient,
-	QueryClientProvider,
-	QueryErrorResetBoundary
-} from 'react-query';
-export { ReactQueryDevtools } from 'react-query/devtools';
 export * from './hooks';
 export * from './types';
+export * from './client';
 export { createService } from './service';
-export { createClient, getClient } from './client';
